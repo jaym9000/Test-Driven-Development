fe17860c8f16217f7eb54e109ccff8e037b31486
Jay-M <jean_marcc@hotmail.com>
2022-05-24 22:49:16 -0300

    Exercise 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,23 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let i =1;
+    let hey = '';
 
+    if(string === ''){
+        return '';
+    }
+
+    while(i<num){
+        string+="hey";
+        i++;
+    }
+
+    if(num < 0){
+        return hey += "ERROR";
+    }else if(num === 0){
+        return '';    
+    }else{
+        return string;
+    }
 };
 
 // Do not edit below this line
