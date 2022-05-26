89c6e2e02c701f94793e1cead4d971329446927f
Jay-M <jean_marcc@hotmail.com>
2022-05-25 22:59:08 -0300

    exercise 4 first test case passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, item) {
+    let index;
+    for(let i = 0; i< arr.length; i++){
+        if(arr[i] == item){
+            index = i;
+        }
+    }
+    arr.splice(index, 1);
+    return arr;
 };
 
 // Do not edit below this line
