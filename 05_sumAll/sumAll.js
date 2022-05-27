25ea78eddcedf97ee2fc1dafcf87dfc6596d9fa9
Jay-M <jean_marcc@hotmail.com>
2022-05-26 22:21:50 -0300

    exercise 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let sum = 0;
 
+    if(numOne < 0 || numTwo < 0 || typeof numOne === 'string' || typeof numTwo === 'string'){ 
+        return "ERROR";
+    }else if(numOne > numTwo){
+        for(let i=numTwo; i<= numOne; i++){
+            sum+=i;
+        }
+    }else if(numOne < numTwo){
+        for(let i=numOne; i<= numTwo; i++){
+            sum+=i;
+        }
+    }else{
+        return "ERROR";
+    }
+    return sum;
 };
 
 // Do not edit below this line
