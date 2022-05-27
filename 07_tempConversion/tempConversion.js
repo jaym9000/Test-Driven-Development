0f6e5638296f1f24d88366cca488b522175aba11
Jay-M <jean_marcc@hotmail.com>
2022-05-26 22:47:35 -0300

    Exercise 7 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(degree) {
+  let result = parseFloat(((degree-32)*0.5556));
+  return Math.round(result * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(degree) {
+  let result = parseFloat(((degree*9)/5)+32);
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
