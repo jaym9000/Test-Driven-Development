b322b6ccc5b15f35e92918160fbfb566fa9c042c
Jay-M <jean_marcc@hotmail.com>
2022-05-26 21:51:30 -0300

    finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,26 @@
-const removeFromArray = function(arr, item) {
+// const removeFromArray = function(arr, item) {
+//     let index;
+//     for(let i = 0; i< arr.length; i++){
+//         if(arr[i] == item){
+//             index = i;
+//         }
+//     }
+//     arr.splice(index, 1);
+//     return arr;
+// };
+
+const removeFromArray = function(arr, ...args) {
     let index;
-    for(let i = 0; i< arr.length; i++){
-        if(arr[i] == item){
-            index = i;
-        }
+    for(let j = 1; j< arguments.length;j++){
+        for(let i = 0; i< arr.length; i++){
+            if(arr[i] == arguments[j]){
+                index = i;
+            }
+         }
+        if(arr.includes(arguments[j]))
+            arr.splice(index, 1);
     }
-    arr.splice(index, 1);
+
     return arr;
 };
 
