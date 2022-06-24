3e8f3d8c51b66496349b91881d9526886376ee8d
Jay-M <jean_marcc@hotmail.com>
2022-06-23 22:32:27 -0300

    finished palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const cut = [...str].reverse().join("");
+  return (
+    cut.toLowerCase().replace(/\W/g, "") ===
+    str.toLowerCase().replace(/\W/g, "")
+  );
 };
 
 // Do not edit below this line
