3c67e4301b3f2a59e9704c0eccc6f96761506236
Jay-M <jean_marcc@hotmail.com>
2022-06-25 15:14:16 -0300

    exercise done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  let a = 1,
+    b = 0,
+    temp,
+    ans;
 
+  if (n < 0) {
+    return "OOPS";
+  }
+
+  while (n >= 0) {
+    temp = a;
+    a = a + b;
+    ans = b;
+    b = temp;
+    n--;
+  }
+
+  return ans;
 };
 
 // Do not edit below this line
