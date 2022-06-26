43ba1c546ca13662783ebc17a57d411f3055ec30
Jay-M <jean_marcc@hotmail.com>
2022-06-25 23:30:22 -0300

    passed first test case, 2 left

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const findTheOldest = people.map((a) => {
+    let ages = a.yearOfDeath - a.yearOfBirth;
+    return ages;
+  });
 
+  let max = Math.max(...findTheOldest);
+
+  return people[findTheOldest.indexOf(max)];
 };
 
 // Do not edit below this line
